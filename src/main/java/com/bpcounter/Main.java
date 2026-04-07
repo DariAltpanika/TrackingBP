e4b395c2c3e116f62ac08a802fa96513208b0cb7
Yoolica <dariawineeee000@gmail.com>
2026-04-07 14:18:55 +0300

    исправлена ошибка зависимости JavaFx, изменена версия проекта и Maven с 22 на 21, убраны настройки Idea и добавлены в gitIgnore. Добавлен тест-код для проверки работы JavaFx

--- src/main/java/com/bpcounter/Main.java
+++ src/main/java/com/bpcounter/Main.java
@@ -1,9 +1,32 @@
 package com.bpcounter;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
+        launch(args);
+    }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        // Создаем простую надпись
+        Label label = new Label("JavaFX работает!");
+
+        // Размещаем надпись на панели
+        StackPane root = new StackPane(label);
+
+        // Создаем сцену размером 400x300
+        Scene scene = new Scene(root, 400, 300);
+
+        // Настраиваем окно
+        stage.setTitle("Тест JavaFX");
+        stage.setScene(scene);
+        stage.show();
     }
 }
