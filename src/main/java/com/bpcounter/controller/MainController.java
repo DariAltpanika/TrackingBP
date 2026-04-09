825c0da6e2e9f646a822e4c0f3c3707e6ec83b79
Yoolica <dariawineeee000@gmail.com>
2026-04-09 14:41:25 +0300

    внесены корректировки в файл main-view.fxml и добавлена инициализация списка задач в класс MainController.java

--- src/main/java/com/bpcounter/controller/MainController.java
+++ src/main/java/com/bpcounter/controller/MainController.java
@@ -1,4 +1,26 @@
 package com.bpcounter.controller;
-public class MainController {
+import com.bpcounter.model.Task;
+import com.bpcounter.model.TaskStatus;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 
+import java.util.HashMap;
+
+public class MainController {
+    private HashMap<Task, TaskStatus> taskMap = new HashMap<>();
+   @FXML private ListView taskList;
+
+    @FXML
+    public void initialize() {
+       hashMapAndTaskListInitialization();
+    }
+
+    //инициализация taskMap
+    private void hashMapAndTaskListInitialization() {
+        for (Task task : Task.values()) {
+            String name = task.getDisplayName();
+            taskList.getItems().add(name);
+            taskMap.put(task, new TaskStatus(task));
+        }
+    }
 }
