a8f1fc40a630ee1a2389444e3f80ad8cd11b1ebb
Yoolica <dariawineeee000@gmail.com>
2026-04-18 17:48:02 +0300

    Очистка репозитория: удалены неиспользуемые файлы, обновлён gitignore

--- src/main/java/com/trackingbp/Main.java
+++ src/main/java/com/trackingbp/Main.java
@@ -7,33 +7,45 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.InputStream;
+import java.net.URL;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // 1. Загружаем FXML файл
-        FXMLLoader loader = new FXMLLoader(
-                getClass().getResource("/com/trackingbp/view/main-view.fxml")
-        );
+        // 1. Загружаем FXML
+        URL fxmlUrl = getClass().getResource("/com/trackingbp/view/main-view.fxml");
+        if (fxmlUrl == null) {
+            System.err.println("FXML not found!");
+            return;
+        }
+        FXMLLoader loader = new FXMLLoader(fxmlUrl);
 
         // 2. Создаем сцену
         Scene scene = new Scene(loader.load(), 800, 600);
-        scene.getStylesheets().add(getClass().getResource("/com/trackingbp/view/style.css").toExternalForm());
 
-        // 3. Настраиваем окно
+        // 3. Загружаем CSS
+        URL cssUrl = getClass().getResource("/com/trackingbp/view/style.css");
+        if (cssUrl != null) {
+            scene.getStylesheets().add(cssUrl.toExternalForm());
+        }
+
+        // 4. Настройки окна
         primaryStage.setTitle("TrackingBP");
 
-        primaryStage.getIcons().add(new Image("/miniIcon.png"));
+        // 5. Загружаем иконку
+        InputStream iconStream = getClass().getResourceAsStream("/miniIcon.png");
+        if (iconStream != null) {
+            primaryStage.getIcons().add(new Image(iconStream));
+        }
 
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
-        primaryStage.setMaximized(false);
         primaryStage.show();
     }
 
     public static void main(String[] args) {
-            launch(args);
+        launch(args);
     }
 }
